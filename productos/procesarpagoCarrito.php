dd53302270743adb42bc25ee8eef6c81dca4d926
Carlos Daniel Quispe Quispe <quispecarlosdaniel078@gmail.com>
2023-04-20 19:28:59 -0500

    git add

--- productos/procesarpagoCarrito.php
+++ productos/procesarpagoCarrito.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 // Incluir archivo de conexión
@@ -11,7 +10,29 @@ $tarjeta = isset($_POST['tarjeta']) ? mysqli_real_escape_string($conexion, $_POS
 $vencimiento = isset($_POST['vencimiento']) ? mysqli_real_escape_string($conexion, $_POST['vencimiento']) : '';
 $cvv = isset($_POST['cvv']) ? mysqli_real_escape_string($conexion, $_POST['cvv']) : '';
 
-// Realizar la transacción para cada producto en el carrito
+// Obtener el ID del usuario (reemplaza este valor por el ID real del usuario logueado)
+$usuario_id = $_SESSION['id_usuario'];
+
+// Calcular el total del pedido
+$total = 0;
+foreach ($_SESSION['carrito'] as $producto_id => $cantidad) {
+    $query_producto = "SELECT * FROM productos WHERE id = $producto_id";
+    $result_producto = mysqli_query($conexion, $query_producto);
+    $producto = mysqli_fetch_assoc($result_producto);
+    $total += $producto['precio'] * $cantidad;
+}
+
+// Insertar el pedido en la tabla `pedidos`
+$query_pedido = "INSERT INTO pedidos (usuario_id, total) VALUES ($usuario_id, $total)";
+$result_pedido = mysqli_query($conexion, $query_pedido);
+
+// Obtener el ID del pedido recién creado
+$pedido_id = mysqli_insert_id($conexion);
+
+// Insertar la transacción en la tabla `transacciones`
+$query_transaccion = "INSERT INTO transacciones (producto_id, precio, nombre, email, tarjeta, vencimiento, cvv, fecha) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
+$stmt_transaccion = $conexion->prepare($query_transaccion);
+
 foreach ($_SESSION['carrito'] as $producto_id => $cantidad) {
     // Obtener información del producto desde la base de datos
     $query_producto = "SELECT * FROM productos WHERE id = $producto_id";
@@ -26,19 +47,31 @@ foreach ($_SESSION['carrito'] as $producto_id => $cantidad) {
     // Obtener el precio del producto
     $precio = $producto['precio'];
 
-    // Obtener la fecha actual
+    // Agregar el detalle del pedido a la base de datos
+    $query_detalle = "INSERT INTO detalles_pedido (pedido_id, producto_id, precio, cantidad) VALUES ($pedido_id, $producto_id, $precio, $cantidad)";
+    $result_detalle = mysqli_query($conexion, $query_detalle);
+
+    // Verificar que el detalle del pedido se guardó correctamente
+    if (!$result_detalle) {
+        // Mostrar mensaje de error
+        die("Error al procesar el pedido");
+    }
     $fecha = date('Y-m-d H:i:s');
-
-    // Agregar la transacción a la base de datos
-    $query_transaccion = "INSERT INTO transacciones  (producto_id, precio, nombre, email, tarjeta, vencimiento, cvv, fecha) VALUES ($producto_id, $precio, '$nombre', '$email', '$tarjeta', '$vencimiento', '$cvv', '$fecha')";
+    $query_transaccion = "INSERT INTO transacciones (producto_id, precio, nombre, email, tarjeta, vencimiento, cvv, fecha) VALUES ($producto_id, $precio, '$nombre', '$email', '$tarjeta', '$vencimiento', '$cvv', '$fecha')";
     $result_transaccion = mysqli_query($conexion, $query_transaccion);
-
+    
     // Verificar que la transacción se realizó correctamente
-    if (!$result_transaccion) {
+    if ($result_transaccion) {
+        // Redirigir a la página de éxito
+        header('Location: pago_exitoso.php');
+        exit();
+    } else {
         // Mostrar mensaje de error
         die("Error al procesar el pago");
     }
 }
+
+$stmt_transaccion->close();
 
 // Limpiar el carrito de compras
 $_SESSION['carrito'] = array();
